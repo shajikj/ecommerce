946a5e1b82bdb8cd1d54e40557c01dae7285bf40
Hp <shajikj81@gmail.com>
2025-11-02 23:02:25 +0530

    To check

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -19,6 +19,7 @@ class Admin extends CI_Controller
     public function index()
     {
         redirect('admin/products');
+        redirect('admin/products');
     }
 
     public function products()
